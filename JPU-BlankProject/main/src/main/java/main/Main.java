98a0825acc0120ec98f426021e1baeffa6ec596b
laetitia <laetitia.kessas@viacesi.fr>
2019-05-23 08:47:59 +0200

    test commit

--- JPU-BlankProject/main/src/main/java/main/Main.java
+++ JPU-BlankProject/main/src/main/java/main/Main.java
@@ -27,6 +27,7 @@ public abstract class Main {
         final View view = new View(model);
         final Controller controller = new Controller(view, model);
         view.setController(controller);
+        //coucou
 
         controller.control();
         controller.orderPerform(ControllerOrder.English);
