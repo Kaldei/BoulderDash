95b3dc6b3028073c93d304ad2f183eeb05313665
Caldei <k4ld3i@gmail.com>
2019-05-29 11:59:57 +0200

    View
    Implementation of class view
    Avoid ViewFrame & ViewPanel

--- JPU-BlankProject/view/src/main/java/view/ViewFrame.java
+++ JPU-BlankProject/view/src/main/java/view/ViewFrame.java
@@ -1,6 +1,5 @@
 package view;
 
-import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -37,77 +36,6 @@ class ViewFrame extends JFrame implements KeyListener {
 	}
 
 	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model the model
-	 * @param gc    the gc
-	 */
-	public ViewFrame(final IModel model, final GraphicsConfiguration gc) {
-		super(gc);
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model the model
-	 * @param title the title
-	 * @throws HeadlessException the headless exception
-	 */
-	public ViewFrame(final IModel model, final String title) throws HeadlessException {
-		super(title);
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Instantiates a new view frame.
-	 *
-	 * @param model the model
-	 * @param title the title
-	 * @param gc    the gc
-	 */
-	public ViewFrame(final IModel model, final String title, final GraphicsConfiguration gc) {
-		super(title, gc);
-		this.buildViewFrame(model);
-	}
-
-	/**
-	 * Gets the controller.
-	 *
-	 * @return the controller
-	 */
-	private IController getController() {
-		return this.controller;
-	}
-
-	/**
-	 * Sets the controller.
-	 *
-	 * @param controller the new controller
-	 */
-	protected void setController(final IController controller) {
-		this.controller = controller;
-	}
-
-	/**
-	 * Gets the model.
-	 *
-	 * @return the model
-	 */
-	protected IModel getModel() {
-		return this.model;
-	}
-
-	/**
-	 * Sets the model.
-	 *
-	 * @param model the new model
-	 */
-	private void setModel(final IModel model) {
-		this.model = model;
-	}
-
-	/**
 	 * Builds the view frame.
 	 *
 	 * @param model the model
@@ -117,7 +45,7 @@ class ViewFrame extends JFrame implements KeyListener {
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.addKeyListener(this);
-		this.setContentPane(new ViewPanel(this));
+		//this.setContentPane(new ViewPanel(this));
 		this.setSize(400 + this.getInsets().left + this.getInsets().right,
 				60 + this.getInsets().top + this.getInsets().bottom);
 		this.setLocationRelativeTo(null);
@@ -158,4 +86,41 @@ class ViewFrame extends JFrame implements KeyListener {
 	public void keyReleased(final KeyEvent e) {
 
 	}
+
+	/**
+	 * Gets the controller.
+	 *
+	 * @return the controller
+	 */
+	private IController getController() {
+		return this.controller;
+	}
+
+	/**
+	 * Sets the controller.
+	 *
+	 * @param controller the new controller
+	 */
+	protected void setController(final IController controller) {
+		this.controller = controller;
+	}
+
+	/**
+	 * Gets the model.
+	 *
+	 * @return the model
+	 */
+	protected IModel getModel() {
+		return this.model;
+	}
+
+	/**
+	 * Sets the model.
+	 *
+	 * @param model the new model
+	 */
+	private void setModel(final IModel model) {
+		this.model = model;
+	}
+
 }
