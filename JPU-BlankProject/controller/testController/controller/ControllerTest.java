bdfb91e5fd2da5f446079d2edb80e4bc6c0c28b4
Antho <anthony.lorendeaux@free.fr>
2019-06-03 09:27:11 +0200

    Update ControllerTest.java

--- JPU-BlankProject/controller/testController/controller/ControllerTest.java
+++ JPU-BlankProject/controller/testController/controller/ControllerTest.java
@@ -16,13 +16,39 @@ import contract.UserOrder;
 
 public class ControllerTest {
 
+	/**
+	 * The view
+	 */
 	private IView view;
 
+	/**
+	 * The model
+	 */
 	private IModel model;
 
+	/**
+	 * The controller
+	 */
 	private Controller controller;
 
+	/**
+	 * stackOrder
+	 */
 	private UserOrder stackOrder = UserOrder.UP;
+	
+	/**
+	 * @return
+	 */
+	public UserOrder getStackOrder() {
+		return stackOrder;
+	}
+
+	/**
+	 * @param stackOrder
+	 */
+	public void setStackOrder(UserOrder stackOrder) {
+		this.stackOrder = stackOrder;
+	}
 
 	@BeforeClass
 	public static void setUpBeforeClass() throws Exception {
@@ -32,26 +58,31 @@ public class ControllerTest {
 	public static void tearDownAfterClass() throws Exception {
 	}
 
+	/**
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
+		// setup a new controller
 		this.controller = new Controller(view, model);
 	}
 
 	@After
 	public void tearDown() throws Exception {
 	}
-
-	@Test
-	public void testController() {
-		// Constructor
-	}
-
+	
+	/**
+	 * test the method Play
+	 */
 	@Test
 	public void testPlay() {
 		// No test available
 		// fail("Not yet implemented");
 	}
 
+	/**
+	 * @throws IOException
+	 */
 	@Test
 	public void testOrderPerform() throws IOException {
 
@@ -60,34 +91,37 @@ public class ControllerTest {
 		assertEquals(expectedUserOrder, this.stackOrder);
 	}
 
+	/**
+	 * test the method GetOrderPerformer
+	 */
 	@Test
 	public void testGetOrderPerformer() {
 		assertNotNull(controller.getOrderPerformer());
 	}
 
+	/**
+	 * Test the method KillPlayer
+	 */
 	@Test
 	public void testKillPlayer() {
 		// No test available
 		// fail("Not yet implemented");
 	}
 
+	/**
+	 * Test the method KillMonster
+	 */
 	@Test
 	public void testKillMonster() {
 		// No test available
 		// fail("Not yet implemented");
 	}
 
+	/**
+	 * Test the method clearOrder
+	 */
 	@Test
 	public void testClearOrder() {
 		//assertEquals(UserOrder.NOP, controller.getOrderPerformer());
 	}
-
-	public UserOrder getStackOrder() {
-		return stackOrder;
-	}
-
-	public void setStackOrder(UserOrder stackOrder) {
-		this.stackOrder = stackOrder;
-	}
-
 }
