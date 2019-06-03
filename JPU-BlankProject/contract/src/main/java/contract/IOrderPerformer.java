a6135a65cd3472e63a8478dd6d9335ae8efb80e9
Antho <anthony.lorendeaux@free.fr>
2019-06-03 09:18:34 +0200

    Javadoc

--- JPU-BlankProject/contract/src/main/java/contract/IOrderPerformer.java
+++ JPU-BlankProject/contract/src/main/java/contract/IOrderPerformer.java
@@ -2,8 +2,16 @@ package contract;
 
 import java.io.IOException;
 
+/**
+ * @author Anthony
+ *
+ */
 public interface IOrderPerformer {
 
+		/**
+		 * @param userOrder
+		 * @throws IOException
+		 */
 		void orderPerform(UserOrder userOrder) throws IOException;
 
 }
