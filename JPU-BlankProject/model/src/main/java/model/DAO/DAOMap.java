4bf539bb0eaf46b6fc902ffbb66e6085de47eca7
Cytzen <caldeireiro.arthur@gmail.com>
2019-09-08 18:52:16 +0200

    Local Files

--- JPU-BlankProject/model/src/main/java/model/DAO/DAOMap.java
+++ JPU-BlankProject/model/src/main/java/model/DAO/DAOMap.java
@@ -17,7 +17,7 @@ public class DAOMap {
 	 * The connection
 	 */
 	private DBConnection MyConnection;
-	private String txt = "C:\\\\Users\\\\User\\\\Documents\\\\GitHub\\\\Project-5\\\\JPU-BlankProject\\\\model\\\\src\\\\main\\\\resources\\\\map.txt";
+	private String txt = "C:\\Users\\calde\\git\\Project-5\\JPU-BlankProject\\model\\src\\main\\resources\\map.txt";
 
 	/**
 	 * Instanciates DAO map
